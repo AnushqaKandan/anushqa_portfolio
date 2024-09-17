7d5f06d5774a01d9afe5cffee7b8fd80b390a2d6
Name <anushqakandan03@gmail.com>
2024-09-17 16:25:14 +0200

    Updates

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,29 +1,40 @@
 <template>
   <div class="container">
-    <div class="row">
-    </div>
+    <div class="row"></div>
 
     <!-- Flexbox for side-by-side layout -->
     <div class="contact-container">
       <!-- Contact Info Card -->
-      <div class="card contact-card text-white" data-aos="fade-right">
+      <div class="card contact-card text-white" 
+           data-aos="fade-right" 
+           data-aos-delay="200" 
+           data-aos-offset="300">
         <h2 class="display-2 text-center">Contact Info</h2>
         <div class="contact-info">
           <div v-if="contact?.length">
             <p><i class="bi bi-telephone-forward-fill"></i> Contact number: {{ contact[0].phone }}</p>
             <p><i class="fa-solid fa-envelope"></i> Email Address: {{ contact[0].email }}</p>
             <p>
-              <a class="icon" href="https://github.com/anushqakandan" target="_blank"><i class="fa-brands fa-github"></i></a>
-              <a class="icon" href="https://www.linkedin.com/in/anushqa-kandan-09379a317/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+              <a class="icon" href="https://github.com/anushqakandan" target="_blank">
+                <i class="fa-brands fa-github"></i>
+              </a>
+              <a class="icon" href="https://www.linkedin.com/in/anushqa-kandan-09379a317/" target="_blank">
+                <i class="fa-brands fa-linkedin"></i>
+              </a>
             </p>
-            <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52893.1530907546!2d18.561739288993632!3d-34.04843507824646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc4721e60ec221%3A0xd3097704b4edb9ac!2sMitchells%20Plain%2C%20Cape%20Town!5e0!3m2!1sen!2sza!4v1726552905113!5m2!1sen!2sza" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
+            <p>
+              <iframe src="https://www.google.com/maps/embed?pb=..." width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </p>
           </div>
           <Spinner v-else />
         </div>
       </div>
 
       <!-- Form -->
-      <div class="card form-card text-white" data-aos="fade-left">
+      <div class="card form-card text-white" 
+           data-aos="fade-left" 
+           data-aos-delay="200" 
+           data-aos-offset="300">
         <h2 class="display-2 text-center">Let's work together</h2>
         <form action="https://formspree.io/f/myzgydyw" method="POST">
           <div class="mb-3">
@@ -67,6 +78,7 @@
     </div>
   </div>
 </template>
+
 
 <script setup>
 import Spinner from './Spinner.vue'
